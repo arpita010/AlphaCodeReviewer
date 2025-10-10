f344781229715c95ce5c3e4bb142113caa1e6408
Arpita Mittal <arpitamittalam66@gmail.com>
2025-10-10 19:27:08 +0530

    Initial Spring Boot repository setup

---
+++ src/main/java/com/app/AIphaCodeReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIphaCodeReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIphaCodeReviewerApplication.class, args);
+	}
+
+}
