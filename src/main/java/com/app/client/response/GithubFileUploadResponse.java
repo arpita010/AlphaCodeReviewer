98c3dff7d4749a6280350d7affc3492966b7c6ea
Arpita Mittal <arpitamittalam66@gmail.com>
2025-10-10 20:57:51 +0530

    feat: Github Client Configured for image uploads

---
+++ src/main/java/com/app/client/response/GithubFileUploadResponse.java
@@ -0,0 +1,11 @@
+package com.app.client.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GithubFileUploadResponse {
+  private GithubContentResponse content;
+  private GithubCommitResponse commit;
+}
